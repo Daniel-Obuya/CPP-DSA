74368ce245939a40ee8a5d46f1e139b1b0491a57
Daniel-Obuya <166502967+Daniel-Obuya@users.noreply.github.com>
2026-04-21 16:13:46 +0300

    Update Question1.cpp

--- Question1.cpp
+++ Question1.cpp
@@ -3,8 +3,10 @@ using namespace std;
 
 int main(){
     int days;
+    // Take input 
     cout << "Enter the number of days: ";
     cin >> days;
+    // Converting hours in days to seconds
     int seconds = days * 24 * 60 * 60;
     cout << "There are " << seconds << " seconds in " << days << " days." << endl;
     return 0;
