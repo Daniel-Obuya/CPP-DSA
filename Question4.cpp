1f025be8aac94447255199460e4b72282051d830
Daniel-Obuya <166502967+Daniel-Obuya@users.noreply.github.com>
2026-04-21 16:20:13 +0300

    Update Question4.cpp

--- Question4.cpp
+++ Question4.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
-#include <cctype> 
+#include <cctype> // For character handling functions like isupper() and islower()
 using namespace std;
 
 int main() {
+    // Declare a variable to store the character input by the user
     char cr;
+    // Prompt the user to enter a character
     cout << "Enter a character: ";
     cin >> cr;
 
+    // Check if the character is uppercase
     if (isupper(cr)) {
         cout << cr << " is in uppercase." << endl;
-    } else if (islower(cr)) {
+    } 
+    // Check if the character is lowercase
+    else if (islower(cr)) {
         cout << cr << " is in lowercase." << endl;
-    } else {
+    }
+    // If it's neither uppercase nor lowercase (e.g., number or symbol)
+    else {
         cout << "Please enter a valid character" << endl;
     }
     return 0;
