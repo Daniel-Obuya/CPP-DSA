9077ed5f4aabf11db07acab06454bba513bf6938
Daniel-Obuya <166502967+Daniel-Obuya@users.noreply.github.com>
2026-04-21 16:15:41 +0300

    Update Question2.cpp

--- Question2.cpp
+++ Question2.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
-#include <cmath>
+#include <cmath> // For mathematical functions like pow() and M_PI
 using namespace std;
 
 int main(){
     int radius;
+    // Prompt the user to enter the radius
     cout << "Enter the radius: ";
     cin >> radius;
 
+    // Calculate the volume of a sphere using the formula:
+    // Volume = (4/3) * π * r^3
     double volume = (4.0/3.0) * M_PI * pow(radius, 3);
+    // Display the calculated volume
     cout << "The volume of the sphere is " << volume << endl;
 
     return 0;
